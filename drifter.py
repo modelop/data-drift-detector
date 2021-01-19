1b0781f3ee0a19729362bf9fd1543b0506a10ebc
Steve Avsec <stephen.avsec@opendatagroup.com>
2021-01-18 21:20:08 -0600

    Initial model

---
+++ drifter.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from scipy.stats import ks_2samp
+from scipy.stats import binom_test
+
+#modelop.init
+def begin():
+    global train, numerical_features
+    train = pd.read_csv('training_data.csv')
+    numerical_features = train.select_dtypes(['int64', 'float64']).columns
+    pass
+
+#modelop.score
+def action(datum):
+    yield datum
+
+#modelop.metrics
+def metrics(data):
+    ks_tests = [ks_2samp(train.loc[:, feat], data.loc[:, feat]) \
+                for feat in numerical_features]
+    pvalues = [x[1] for x in ks_tests]
+    ks_pvalues = dict(zip(f"{numerical_features}_p-value", pvalues))
+    
+    yield ks_pvalues
