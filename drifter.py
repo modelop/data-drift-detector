83ec063459d42d080cb63bedf60fe5e989253960
Steve Avsec <stephen.avsec@opendatagroup.com>
2021-01-19 13:02:45 -0600

    Fixing error in flattened JSON

--- drifter.py
+++ drifter.py
@@ -19,6 +19,7 @@ def metrics(data):
     ks_tests = [ks_2samp(train.loc[:, feat], data.loc[:, feat]) \
                 for feat in numerical_features]
     pvalues = [x[1] for x in ks_tests]
-    ks_pvalues = dict(zip(f"{numerical_features}_p-value", pvalues))
+    list_of_pval = [f"{feat}_p-value" for feat in numerical_features]
+    ks_pvalues = dict(zip(list_of_pval, pvalues))
     
     yield ks_pvalues
